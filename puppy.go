06fe5cfb77ff70e82d83517fd2453250df6320c4
povilass <povilas@amlyze.com>
2023-12-25 20:36:56 +0200

    fix: just

--- puppy.go
+++ puppy.go
@@ -25,10 +25,10 @@ func From11() {
 	fmt.Println("I'm from version 1.1.0")
 }
 
-/*func From12() {
+func From12() {
 	fmt.Println("I'm from version 1.2.0")
 }
 
-func From13() {
-	fmt.Println("I'm from version 1.3.0")
-}*/
+//func From13() {
+//	fmt.Println("I'm from version 1.3.0")
+//}
