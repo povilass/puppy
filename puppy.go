6e15571b5cd86584224bf6600e23475e32a98ff4
povilass <povilas@amlyze.com>
2023-12-25 20:35:52 +0200

    fix: just

--- puppy.go
+++ puppy.go
@@ -1,6 +1,7 @@
 package puppy
 
 import (
+	"fmt"
 	"github.com/GoesToEleven/dog"
 )
 
@@ -19,3 +20,15 @@ func BigBark() string {
 func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
 }
+
+func From11() {
+	fmt.Println("I'm from version 1.1.0")
+}
+
+/*func From12() {
+	fmt.Println("I'm from version 1.2.0")
+}
+
+func From13() {
+	fmt.Println("I'm from version 1.3.0")
+}*/
